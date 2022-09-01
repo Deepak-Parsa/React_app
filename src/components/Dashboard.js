a48a5360e46ce13ada810c3bef3897b09f7c7556
Deepak Kumar <deepakiimt004@gmail.com>
2022-09-01 11:20:19 +0530

    modify Dashboard.js

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -5,7 +5,7 @@ const Dashboard = () => {
         <>
             <div className='bg-gray-500 w-full h-14'>
                 <div className='flex justify-between pt-4'>
-                    <h1 className='pl-8 text-white text-2xl'>MY WEBSITE</h1>
+                    <h1 className='pl-8 font-extrabold text-transparent text-2xl bg-clip-text bg-gradient-to-r from-purple-400 to-yellow-200 ' style={{ fontFamily: "Audiowide" }}>MY WEBSITE</h1>
                     <ul className='flex '>
                         <li className='mx-4 text-white cursor-pointer'>Home</li>
                         <li className='mx-4 text-white cursor-pointer'>About</li>
@@ -14,6 +14,7 @@ const Dashboard = () => {
                     </ul>
                 </div>
             </div>
+
         </>
     )
 }
